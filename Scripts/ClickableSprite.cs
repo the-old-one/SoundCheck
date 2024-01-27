5bdf5f4e3769812475aafb6e9f27d447e33c57e1
Oleg Chernyshenko <ochernyshenko@transurban.com>
2024-01-27 13:55:28 +1000

    Add more animations

--- Scripts/ClickableSprite.cs
+++ Scripts/ClickableSprite.cs
@@ -12,6 +12,7 @@ public partial class ClickableSprite : Area2D
 	private Vector2 originalScale;
 
 	private AnimatedSprite2D sprite;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -20,8 +21,16 @@ public partial class ClickableSprite : Area2D
 		if (sprite != null)
 		{
 			originalScale = sprite.Scale;
+			sprite.AnimationFinished += OnActivaAnimationFinished;
 		}
 		SetActive(IsActive, 0f);
+		// find FMODController
+		Callable callable = new Callable(this, MethodName.OnMarkerSignal);
+
+		var fmodController = GetNode<Node2D>("/root/Main/FMODController");
+		
+		fmodController.Connect("marker_called", callable);
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,7 +42,6 @@ public partial class ClickableSprite : Area2D
 	{
 		if (@inputEvent.IsActionPressed("click"))
 		{
-			GD.Print("Click");
 			toggleActive();
 		}
 	}
@@ -54,7 +62,7 @@ public partial class ClickableSprite : Area2D
 			Tween tween = GetTree().CreateTween().SetParallel(true);
 			tween.TweenProperty(sprite, "modulate", new Color(1, 1, 1, 1), transitionDuration).SetTrans(Tween.TransitionType.Sine);
 			tween.TweenProperty(sprite, "scale", new Vector2(originalScale.X + 0.1f, originalScale.Y + 0.1f), transitionDuration).SetTrans(Tween.TransitionType.Sine);
-			sprite.Play("active");
+			
 		}
 		else
 		{
@@ -64,5 +72,24 @@ public partial class ClickableSprite : Area2D
 			tween.TweenProperty(sprite, "scale", originalScale, transitionDuration).SetTrans(Tween.TransitionType.Sine);
 			sprite.Play("idle");
 		}
+	}
+	
+	private void OnMarkerSignal(string markerName)
+	{
+		// GD.Print("markerName: " + markerName + " TrackName: " + TrackName);
+		if (markerName == TrackName + "_sing")
+		{
+			
+			if (!IsActive) return;
+			GD.Print("Matched");
+			sprite.Stop();
+			sprite.Play("active");
+			// toggleActive();
+		}
+	}
+	
+	private void OnActivaAnimationFinished()
+	{
+		sprite.Play("idle");
 	}
 }
--- Scripts/FMODController.gd
+++ Scripts/FMODController.gd
@@ -1,29 +1,34 @@
 extends Node2D
 
 @export var event: EventAsset
-var instance: EventInstance
-var callable: Callable = Callable(self, "beat_callback")
 
-# Called when the node enters the scene tree for the first time.
+var instance: EventInstance
+var marker_callable: Callable = Callable(self, "marker_callback")
+
+signal marker_called(marker_name)
+
+const BIRD_1 = "bird_1"
+const BIRD_2 = "bird_2"
+const BIRD_3 = "bird_3"
+const BIRD_BASS = "bird_bass"
+
 func _ready():
 	instance = FMODRuntime.create_instance(event)
-	instance.set_callback(callable, FMODStudioModule.FMOD_STUDIO_EVENT_CALLBACK_TIMELINE_BEAT)
-
 	instance.start()
-	
+	instance.set_callback(marker_callable, FMODStudioModule.FMOD_STUDIO_EVENT_CALLBACK_TIMELINE_MARKER)
+
 # 	instance.set_parameter_by_name_with_label("bird_1", "On", false)
 
 func _on_track_toggle(track_name, is_on):
-	
-	# set is_on to "On" if true and "Off" if false
 	var state = "On" if is_on else "Off"
 	print(track_name, " ", state)
 	instance.set_parameter_by_name_with_label(track_name, state, false)
-	
-func beat_callback(args):
-	if args.properties.beat:
-		print("beat!")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+func marker_callback(args):
+# 	print("Marker: " + args.properties.name + " at position: " + str(args.properties.position))
+	var marker_name = args.properties.name
+# 	marker_called.emit(marker_name)
+	call_deferred("emit_signal","marker_called", marker_name)
+# 	if args.properties.tempo:
+# 		print("bar!", args.properties.tempo)

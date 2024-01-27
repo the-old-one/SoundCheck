7da2a34fbabbcd4ead06218f7cdf4d79217feb6f
Oleg Chernyshenko <ochernyshenko@transurban.com>
2024-01-27 20:34:08 +1000

    More art

--- Scripts/FMODController.gd
+++ Scripts/FMODController.gd
@@ -5,7 +5,10 @@ extends Node2D
 var instance: EventInstance
 var marker_callable: Callable = Callable(self, "marker_callback")
 
+var active_tracks = 0
+
 signal marker_called(marker_name)
+signal active_tracks_number_changed(active_tracks_number)
 
 const BIRD_1 = "bird_1"
 const BIRD_2 = "bird_2"
@@ -20,9 +23,17 @@ func _ready():
 # 	instance.set_parameter_by_name_with_label("bird_1", "On", false)
 
 func _on_track_toggle(track_name, is_on):
+	if is_on == true:
+		active_tracks += 1
+# 		state = "On"
+	else:
+		active_tracks -= 1
+# 		state = "Off"
+	print(active_tracks)
 	var state = "On" if is_on else "Off"
 	print(track_name, " ", state)
 	instance.set_parameter_by_name_with_label(track_name, state, false)
+	emit_signal("active_tracks_number_changed", active_tracks)
 
 
 func marker_callback(args):
